b16b289a9429e9780d7221438450a3b3568b3e5b
yash <yaswanthpolepeddi675@gmail.com>
2021-08-01 16:49:44 +0530

    even or odd

---
+++ even and odd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    printf("enter a number");
+    scanf("%d",&i);
+    if(i%2 ==0)
+    printf("the number is even");
+    else
+    printf("the number is odd");
+
+}
