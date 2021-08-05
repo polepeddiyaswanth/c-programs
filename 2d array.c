28e6ed7e5cb5c988474262a8c23d1786fd8c064b
yash <yaswanthpolepeddi675@gmail.com>
2021-08-06 01:45:50 +0530

    CPROGRAM

--- 2d array.c
+++ 2d array.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-   int i,j,n1,n2,disp[100][100],ch,sum,x,row,col,flag=0,m1,m2,a,c;
+   int i,j,n1,n2,disp[100][100],ch,sum,x,row,col,flag=0,m1,m2,a,c,k;
   char var;
    printf("enter the value of rows:");
    scanf("%d",&n1);
@@ -16,7 +16,7 @@ int main()
       }
    }
    programme:
-   printf("\nDo you want to print:\n1:index value\n2:matrix\n3.sum of all elements\n4.enter the number in the matrix it will show position\n5.if you want to replace any element in matrix\n6.if you want to count the number how many times repeated\n");
+   printf("\nDo you want to print:\n1:index value\n2:matrix\n3.sum of all elements\n4.enter the number in the matrix it will show position\n5.if you want to replace any element in matrix\n6.if you want to count the number how many times repeated\n7.to write the matrix in ascending order.\n8.to write the matrix in descending order.\n");
     scanf (" %d", &ch);
     switch(ch)
     {
@@ -98,14 +98,72 @@ int main()
      printf("what is the value you want to count:");
      scanf("%d",&a);
      for(i=0; i<n1; i++)
+       {
+      for(j=0;j<n2;j++) 
+          {
+          if(disp[i][j]==a)
+           c++;
+            }
+   }
+      printf("count=%d",c);
+      break;
+     case 7:
+     printf("after ascending order\n");
+for (i = 0; i < n1; ++i)
+    {
+        for (j = 0; j < n2; ++j)
+        {
+            for (k =(j + 1); k < n2; ++k)
+            {
+                if (disp[i][j] > disp[i][k])
+                {
+                    a = disp[i][j];
+                    disp[i][j] = disp[i][k];
+                    disp[i][k] = a;
+                }
+            }
+        }
+    }
+          for(i=0; i<n1; i++)
    {
       for(j=0;j<n2;j++) 
       {
-          if(disp[i][j]==a)
-           c++;
+         printf("%d ", disp[i][j]);
+         if(j==n2-1)
+         {
+            printf("\n");
+         }
       }
-   }
-      printf("count=%d",c);
+   }     
+      break;
+     case 8:
+      printf("after descending order:\n");
+      for (i = 0; i < n1; ++i)
+    {
+        for (j = 0; j < n2; ++j)
+        {
+            for (k =(j + 1); k < n2; ++k)
+            {
+                if (disp[i][j] < disp[k][j])
+                {
+                    a = disp[i][j];
+                    disp[i][j] = disp[i][k];
+                    disp[i][k] = a;
+                }
+            }
+        }
+    }
+    for(i=0; i<n1; i++)
+   {
+      for(j=0;j<n2;j++) 
+      {
+         printf("%d ", disp[i][j]);
+         if(j==n2-1)
+         {
+            printf("\n");
+         }
+      }
+   }     
       break;
     default:
           printf("pls enter 1 or 2");
